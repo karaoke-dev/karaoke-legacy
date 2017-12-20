7d19e23c4e8de81a8f421c7525383040a7d15e48
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2017-12-20 21:20:13 +0900

    edit time textbox

--- osu.Game.Rulesets.Karaoke/Edit/Dialog/Pieces/TimeTextBox.cs
+++ osu.Game.Rulesets.Karaoke/Edit/Dialog/Pieces/TimeTextBox.cs
@@ -12,6 +12,38 @@ namespace osu.Game.Rulesets.Karaoke.Edit.Dialog.Pieces
     /// </summary>
     public class TimeTextBox : FocusedTextBox
     {
+        public double _timeValue;
+        public double TimeValue
+        {
+            get => _timeValue;
+            set
+            {
+                _timeValue = value;
+                this.Text = DoubleToString(_timeValue);
+            }
+        }
 
+        public override string Text
+        {
+            get => base.Text;
+            set
+            {
+                base.Text = value;
+
+            }
+        }
+
+        protected string DoubleToString(double time)
+        {
+            return TimeSpan.FromMilliseconds(time).ToString(@"mm\:ss\:fff");
+        }
+
+        protected double StringToDouble(string stringTime)
+        {
+            TimeSpan time;
+            TimeSpan.TryParse(stringTime,out time);
+            double milliSecond= time.Milliseconds
+            return milliSecond;
+        }
     }
 }
