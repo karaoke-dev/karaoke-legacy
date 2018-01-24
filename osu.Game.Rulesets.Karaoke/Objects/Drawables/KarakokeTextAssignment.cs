0429fd3fae66db0c8aeb65e24d6137b6f31b1ea9
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-01-24 21:38:32 +0900

    add KarakokeTextAssignment use to assign each text position

---
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/KarakokeTextAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Rulesets.Karaoke.Objects.Drawables
+{
+    /// <summary>
+    /// use to assign text in which place
+    /// 
+    /// subtext (subtext / hidden)
+    /// mainText (lyric / romaji )
+    /// bottomText (lyric / romaji / hidden)
+    /// translateText (translate / hidden)
+    /// </summary>
+    public class KarakokeTextAssignment
+    {
+    }
+}
