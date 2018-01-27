3ea3e113df18b50109c163029f34eec5167b75b6
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-01-27 12:47:55 +0900

    create KaraokeLyricPlayField and KaraokeTonePlayfield

---
+++ osu.Game.Rulesets.Karaoke/UI/PlayField/KaraokeLyricPlayField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.Karaoke.UI.PlayField
+{
+    /// <summary>
+    /// use to manage karaoke lyric's position arrangement
+    /// </summary>
+    public class KaraokeLyricPlayField : Playfield
+    {
+
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/UI/PlayField/KaraokeTonePlayfield.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Game.Rulesets.UI.Scrolling;
+
+namespace osu.Game.Rulesets.Karaoke.UI.PlayField
+{
+    /// <summary>
+    /// use to show karaoke tone Playfield
+    /// like : 
+    /// ---------------------------#####
+    /// --------------#####----####-----
+    /// ---------#####-----####---------
+    /// ---######-----------------------
+    /// --------------------------------
+    /// </summary>
+    public class KaraokeTonePlayfield : ScrollingPlayfield
+    {
+
+        public KaraokeTonePlayfield()
+            : base(ScrollingDirection.Right)
+        {
+        }
+    }
+}
