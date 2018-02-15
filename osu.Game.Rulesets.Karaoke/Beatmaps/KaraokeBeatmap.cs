2cc57a2c343f0498c96edb985509f66d6b757a43
andy840119 <andy840119@gmail.com>
2018-02-15 13:13:10 +0800

    seperate karaoke beatmep property

--- osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmap.cs
+++ osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeBeatmap.cs
@@ -8,15 +8,61 @@ namespace osu.Game.Rulesets.Karaoke.Beatmaps
 {
     /// <summary>
     /// karaoke beatmap
+    /// will contain the basic karaoke song info
     /// </summary>
     public class KaraokeBeatmap
     {
+
+        #region SongInfo
+        /// <summary>
+        /// song name
+        /// </summary>
         public string SongName { get; set; }
 
+        /// <summary>
+        /// song author
+        /// </summary>
         public string AuthorName { get; set; }
 
-        public List<string> Languages { get; set; } = new List<string>();
-
+        /// <summary>
+        /// list singer
+        /// </summary>
         public List<KaraokeSinger> ListSinger { get; set; } = new List<KaraokeSinger>();
+
+        /// <summary>
+        /// song languages
+        /// </summary>
+        public List<string> SongLanguages { get; set; } = new List<string>();
+        #endregion
+
+        #region Language
+
+        /// <summary>
+        /// translate languages
+        /// </summary>
+        public List<string> TranslateLanguages { get; set; } = new List<string>();
+
+        #endregion
+
+        #region Copyright
+
+        /// <summary>
+        /// if this song is sample, means it is not ranked
+        /// </summary>
+        public bool IsSample { get; set; } = false;
+
+        /// <summary>
+        /// karaoke songs can be edit or play with V2 system 
+        /// if contain Authorized code
+        /// </summary>
+        public string AuthCode { get; set; }
+
+        /// <summary>
+        /// if upload beatmap it will auto-generate this
+        /// [reserve]
+        /// </summary>
+        public string PublishKey { get; set; }
+
+        #endregion
     }
 }
