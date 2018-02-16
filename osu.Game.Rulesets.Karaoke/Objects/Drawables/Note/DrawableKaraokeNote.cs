77a8927d63aa6ea11a6d88f8375f49cda1e1f912
andy840119 <andy840119@gmail.com>
2018-02-16 15:36:18 +0800

    separate object/Drawable into karaokeLyric and Note two parts

---
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/Note/DrawableKaraokeNote.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace osu.Game.Rulesets.Karaoke.Objects.Drawables.Note
+{
+    public class DrawableKaraokeNote
+    {
+        public DrawableKaraokeNote()
+        {
+        }
+    }
+}
