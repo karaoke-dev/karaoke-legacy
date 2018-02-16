b8207d32803f4af06cb15d1d428d238124ea01fc
andy840119 <andy840119@gmail.com>
2018-02-16 16:11:33 +0800

    comment DrawableKaraokeNote

--- osu.Game.Rulesets.Karaoke/Objects/Drawables/Note/DrawableKaraokeNote.cs
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/Note/DrawableKaraokeNote.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 namespace osu.Game.Rulesets.Karaoke.Objects.Drawables.Note
 {
+    /// <summary>
+    /// just copy DrawableManiaNote.cs
+    /// and implement 
+    /// 1. TextSets
+    /// 2. singer
+    /// </summary>
     public class DrawableKaraokeNote
     {
         public DrawableKaraokeNote()
