8837347642cc5708ea558fd67c030b8703e00624
andy840119 <andy840119@gmail.com>
2018-02-16 16:06:24 +0800

    depart karaoke text sets from TextsAndMask

---
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/Pieces/TextSets.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace osu.Game.Rulesets.Karaoke.Objects.Drawables.Pieces
+{
+    /// <summary>
+    /// this class contains 
+    /// 1. top text
+    /// 2. main text
+    /// 3. bottom text
+    /// </summary>
+    public class TextSets
+    {
+        public TextSets()
+        {
+        }
+    }
+}
