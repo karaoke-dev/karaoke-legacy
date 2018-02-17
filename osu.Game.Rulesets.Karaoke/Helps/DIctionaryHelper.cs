abe37a7619f02215b05145bf082135f711402dab
andy840119 <andy840119@gmail.com>
2018-02-17 18:12:56 +0800

    Create DIctionaryHelper.cs and add  ResharpDictionaryId(Dic,Dic) Method

---
+++ osu.Game.Rulesets.Karaoke/Helps/DIctionaryHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Rulesets.Karaoke.Helps
+{
+    public class DIctionaryHelper
+    {
+        /// <summary>
+        /// use this method to re-assign new ID(Key),sort from small to large
+        /// </summary>
+        /// <typeparam name="U"></typeparam>
+        /// <typeparam name="V"></typeparam>
+        /// <param name="sourceDictionary"></param>
+        /// <param name="targetDictionary"></param>
+        /// <returns></returns>
+        public static bool ResharpDictionaryId<U,V>(Dictionary<int, U> sourceDictionary, Dictionary<int, V> targetDictionary) where U: struct where V : struct 
+        {
+            return false;
+        }
+    }
+}
