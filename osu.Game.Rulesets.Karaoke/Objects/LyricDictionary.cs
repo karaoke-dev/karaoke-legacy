08c8389f9a76da82db57a47c90b5bb508d9495d8
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-02-28 14:26:34 +0900

    implement LyricDictionary.cs

---
+++ osu.Game.Rulesets.Karaoke/Objects/LyricDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Rulesets.Karaoke.Objects
+{
+    /// <summary>
+    /// Lyric Dictionary
+    /// </summary>
+    /// <typeparam name="Key"></typeparam>
+    /// <typeparam name="Value"></typeparam>
+    public class LyricDictionary<Key,Value> : Dictionary<Key, Value>
+    {
+        /// <summary>
+        /// if key already exist, 'won;t be replaced
+        /// </summary>
+        /// <param name="sourceDictionary"></param>
+        public void Fill(Dictionary<Key, Value> sourceDictionary)
+        {
+            foreach (var single in sourceDictionary)
+            {
+                if(!this.ContainsKey(single.Key))
+                {
+                    Add(single.Key,single.Value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// if key already exist, will be replaced
+        /// </summary>
+        /// <param name="sourceDictionary"></param>
+        public void Replace(Dictionary<Key, Value> sourceDictionary)
+        {
+            foreach (var single in sourceDictionary)
+            {
+                //if exist then remove
+                if (this.ContainsKey(single.Key))
+                    Remove(single.Key);
+
+                Add(single.Key, single.Value);
+            }
+        }
+
+        /// <summary>
+        /// clean all value and fill all the value from sourceDictionary
+        /// </summary>
+        /// <param name="sourceDictionary"></param>
+        public void CleanAndReplace(Dictionary<Key, Value> sourceDictionary)
+        {
+            Clear();
+
+            //fill
+            Fill(sourceDictionary);
+        }
+    }
+}
