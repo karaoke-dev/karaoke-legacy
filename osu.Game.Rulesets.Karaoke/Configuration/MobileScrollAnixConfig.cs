3e16c1253dbe6582482bb43752e3ce1079d42d13
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-13 01:22:44 +0900

    1. add readme.md 2. add MobileScrollAnixConfig for saving config 3. 喵

---
+++ osu.Game.Rulesets.Karaoke/Configuration/MobileScrollAnixConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Rulesets.Karaoke.Configuration
+{
+    /// <summary>
+    /// X or Y-anix can be use as...
+    /// </summary>
+    public enum MobileScrollAnix
+    {
+        /// <summary>
+        /// Time
+        /// </summary>
+        Time,
+
+        /// <summary>
+        /// Volumn
+        /// </summary>
+        Volumn,
+
+        /// <summary>
+        /// Dim
+        /// </summary>
+        BackgroundDim,
+
+        /// <summary>
+        /// Tone
+        /// </summary>
+        Tome,
+
+        /// <summary>
+        /// Speed
+        /// </summary>
+        Speed,
+    }
+
+    /// <summary>
+    /// Config
+    /// </summary>
+    public class MobileScrollAnixConfig
+    {
+        /// <summary>
+        /// Anix
+        /// </summary>
+        public MobileScrollAnix MobileScrollAnix { get; set; }
+
+        /// <summary>
+        /// Sensitive
+        /// </summary>
+        public double Sensitive { get; set; }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/UI/PlayField/README.md
@@ -0,0 +1,2 @@
+﻿this namespace is to place the "Full screen" component
+or can be seperate as a "individual part"
