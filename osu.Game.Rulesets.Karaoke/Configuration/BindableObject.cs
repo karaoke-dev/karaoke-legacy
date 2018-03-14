5e6682a7fdefc589951a6619e813ef908a3162ac
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-15 01:15:28 +0900

    create BindingObject, which can let class object binding and save the value

---
+++ osu.Game.Rulesets.Karaoke/Configuration/BindableObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+using osu.Framework.Configuration;
+
+namespace osu.Game.Rulesets.Karaoke.Configuration
+{
+    public class BindableObject<T> : Bindable<T>
+        where T : class 
+    {
+        public BindableObject(T value)
+            : base(value)
+        {
+
+        }
+
+        public static implicit operator T(BindableObject<T> value) => value?.Value ?? throw new InvalidCastException($"Casting a null {nameof(BindableObject<T>)} to a bool is likely a mistake");
+
+        public override string ToString()
+        {
+            string bindableValue = JsonConvert.SerializeObject(Value);
+            return bindableValue;
+        }
+
+        public override void Parse(object input)
+        {
+            Value = JsonConvert.DeserializeObject<T>(input as string);
+        }
+    }
+}
