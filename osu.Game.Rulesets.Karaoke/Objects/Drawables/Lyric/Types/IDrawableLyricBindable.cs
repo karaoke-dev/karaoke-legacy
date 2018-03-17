da73c408b8a1eb64cc869c481125fe42fa4633e9
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-18 01:33:20 +0900

    新增兩個interface

---
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/Lyric/Types/IDrawableLyricBindable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Framework.Configuration;
+using osu.Game.Rulesets.Karaoke.Configuration;
+
+namespace osu.Game.Rulesets.Karaoke.Objects.Drawables.Lyric.Types
+{
+    /// <summary>
+    /// All the bindable things
+    /// </summary>
+    public interface IDrawableLyricBindable
+    {
+        /// <summary>
+        /// Style
+        /// </summary>
+        BindableObject<KaraokeLyricConfig> Style { get; set; }
+
+        /// <summary>
+        /// Template
+        /// </summary>
+        BindableObject<LyricTemplate> Template { get; set; }
+
+        /// <summary>
+        /// SingerTemplate
+        /// </summary>
+        BindableObject<SingerTemplate> SingerTemplate { get; set; }
+
+        /// <summary>
+        /// TranslateCode
+        /// </summary>
+        Bindable<TranslateCode> TranslateCode { get; set; }
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Objects/Drawables/Lyric/Types/IDrawableLyricParameter.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
+// Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
+
+using osu.Framework.Configuration;
+using osu.Game.Rulesets.Karaoke.Configuration;
+using osu.Game.Rulesets.Karaoke.Objects.Drawables.Lyric.Pieces;
+
+namespace osu.Game.Rulesets.Karaoke.Objects.Drawables.Lyric.Types
+{
+    public interface IDrawableLyricParameter
+    {
+
+        /// <summary>
+        /// Object
+        /// </summary>
+        Objects.Lyric Lyric { get; }
+
+        /// <summary>
+        /// singer
+        /// </summary>
+        Singer Singer { get; set; }
+
+        /// <summary>
+        /// set preemptive time
+        /// </summary>
+        double PreemptiveTime { get; set; }
+
+        /// <summary>
+        /// translate text
+        /// </summary>
+        TranslateString TranslateText { get; set; }
+    }
+}
