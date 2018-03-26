000747e8b77012fd5299a30683198623aec576e2
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-26 23:25:25 +0900

    add a new mod

---
+++ osu.Game.Rulesets.Karaoke/Mods/Mod2018/KaraokeModPDUMCWAMFUW.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using osu.Game.Graphics;
+using osu.Game.Rulesets.Mods;
+
+namespace osu.Game.Rulesets.Karaoke.Mods.Mod2018
+{
+    /// <summary>
+    /// Window$
+    /// </summary>
+    public class KaraokeModPDUMCWAMFUW : Mod
+    {
+        public override string Name => "PDUMCWAMFUW";
+        public override string ShortenedName => "PDUMCWAMFUW";
+        public override double ScoreMultiplier => 1;
+        public override string Description => "I already pay the money, PLEASE DONT UPGRADE MY COMPUTER WITHOUT ASKING FUXK You Window$.";
+        public override FontAwesome Icon => FontAwesome.fa_osu_mod_suddendeath;
+        public override bool Ranked => true;
+    }
+}
