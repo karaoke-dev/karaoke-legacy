8ef8b4b6c48285b6d954bf7442964afa15ec28ba
andy840119 <andy840119@gmail.com>
2018-03-27 13:32:11 +0800

    新增mod 抽卡(乾

---
+++ osu.Game.Rulesets.Karaoke/Mods/Types/IHasSsr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu.Game.Rulesets.Karaoke.Mods.Types
+{
+    /// <summary>
+    /// SSR
+    /// </summary>
+    public interface IHasSsr
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        string SsrAchieveName { get; set; }
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        string SsrId { get; set; }
+
+        /// <summary>
+        /// Precentage
+        /// </summary>
+        string SsrPrecetage { get; set; }
+    }
+}
