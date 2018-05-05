a01f72a25cdac5bc16a8a2fbbbd1825397436e48
為什麼 <andy840119@gmail.com>
2018-05-05 23:47:26 +0900

    no message

--- osu.Game.Rulesets.Karaoke/Service/Lyric/MusixMatchLyric.cs
+++ osu.Game.Rulesets.Karaoke/Service/Lyric/MusixMatchLyric.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace osu.Game.Rulesets.Karaoke.Service.Lyric
 {
+    /// <summary>
+    /// https://developer.musixmatch.com/plans
+    /// Due to Free plan can only show just 30% of the lyric
+    /// So this Service will be implement until i have enough money to pay
+    /// Maybe
+    /// </summary>
     public class MusixMatchLyric : ILyricService
     {
+        public MusixMatchLyric()
+        {
+
+        }
     }
 }
