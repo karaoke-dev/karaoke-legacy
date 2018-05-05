dbca965e60de1aad108239d3bfb8f453a3f2e515
為什麼 <andy840119@gmail.com>
2018-05-05 20:53:51 +0900

    add service class

---
+++ osu.Game.Rulesets.Karaoke/Service/Lyric/LyricServiceProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Game.Rulesets.Karaoke.Service.Lyric
+{
+    public class LyricServiceProvider
+    {
+        /// <summary>
+        /// Get translator by providerType
+        /// </summary>
+        /// <param name="providerType"></param>
+        /// <returns></returns>
+        public static ILyricService GetLyricServiceByProvider(LyricServiceProviderType providerType)
+        {
+            switch (providerType)
+            {
+                case LyricServiceProviderType.MusixMatch:
+                    return new MusixMatchLyric();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public enum LyricServiceProviderType
+    {
+        /// <summary>
+        /// The romaji server just for osu!Karaoke
+        /// <see cref="MusixMatchLyric"/>
+        /// </summary>
+        MusixMatch,
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/Service/Lyric/MusixMatchLyric.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace osu.Game.Rulesets.Karaoke.Service.Lyric
+{
+    public class MusixMatchLyric : ILyricService
+    {
+    }
+}
