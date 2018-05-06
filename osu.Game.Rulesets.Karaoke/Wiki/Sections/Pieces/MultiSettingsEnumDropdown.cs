fd7d0cb59db237366275e213086e0600e4043a37
為什麼 <andy840119@gmail.com>
2018-05-07 01:09:18 +0900

    喵

--- osu.Game.Rulesets.Karaoke/Wiki/Sections/Pieces/MultiSettingsEnumDropdown.cs
+++ osu.Game.Rulesets.Karaoke/Wiki/Sections/Pieces/MultiSettingsEnumDropdown.cs
@@ -2,7 +2,14 @@
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
 using System.Collections.Generic;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Game.Graphics.UserInterface;
+using osu.Game.Overlays.Settings;
+using osu.Game.Rulesets.Karaoke.Configuration;
+using OpenTK;
+using Symcol.Rulesets.Core.Wiki;
 
 namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
 {
@@ -16,6 +23,7 @@ namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
         //List selection
         protected List<DropDownSelectionSection<T>> ListSelectin = new List<DropDownSelectionSection<T>>();
 
+        //public BindableObject<Dictionary<int,T>> Bindnig { get; set; } = new BindableObject<Dictionary<int, T>>(new Dictionary<int, T>());
 
         public string AlretMessage { get; set; } = nameof(T) + "Cannot be same";
 
@@ -47,6 +55,41 @@ namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
     /// </summary>
     public class DropDownSelectionSection<T> : FillFlowContainer
     {
+        protected SettingsDropdown<T> Dropdown;
+        protected WikiTextSection TextSection;
+
+        public Bindable<T> Bindable => Dropdown.Bindable;
+
+        public DropDownSelectionSection()
+        {
+            Direction = FillDirection.Horizontal;
+
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Position = new Vector2(-10, 0),
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    AutoSizeAxes = Axes.Y,
+                    Width = 200,
+                    Child = Dropdown = new SettingsDropdown<T>
+                    {
+
+                    }
+                },
+                new Container
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Width = 400,
+                    AutoSizeAxes = Axes.Y,
+                    AutoSizeDuration = 100,
+                    AutoSizeEasing = Easing.OutQuint,
+                    Child = TextSection = new WikiTextSection("before setting, you have to press this button to enable microphone")
+                }
+            };
+        }
     }
 
     /// <summary>
@@ -54,6 +97,26 @@ namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
     /// </summary>
     public class AlertSection : FillFlowContainer
     {
+        protected WikiTextSection TextSection;
+
+        public AlertSection()
+        {
+            Direction = FillDirection.Horizontal;
+
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Width = 400,
+                    AutoSizeAxes = Axes.Y,
+                    AutoSizeDuration = 100,
+                    AutoSizeEasing = Easing.OutQuint,
+                    Child = TextSection = new WikiTextSection("before setting, you have to press this button to enable microphone")
+                }
+            };
+        }
     }
 
     /// <summary>
@@ -61,5 +124,28 @@ namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
     /// </summary>
     public class AddMoreDropDownSelectionSection : FillFlowContainer
     {
+        protected OsuButton Button;
+
+        public AddMoreDropDownSelectionSection()
+        {
+            Direction = FillDirection.Horizontal;
+
+            Children = new Drawable[]
+            {
+                new Container
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    Width = 400,
+                    AutoSizeAxes = Axes.Y,
+                    AutoSizeDuration = 100,
+                    AutoSizeEasing = Easing.OutQuint,
+                    Child = Button = new OsuButton
+                    {
+                        Text = "Add Section"
+                    }
+                }
+            };
+        }
     }
 }
