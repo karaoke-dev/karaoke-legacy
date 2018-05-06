f020fc0dd5a3223d08024173f47697a707438868
andy840119 <andy840119@gmail.com>
2018-05-06 18:07:02 +0800

    create multi-selection class maybe use in 1. romaji service provider type 2. translate service provider type 3. lyric service provider type(not implement yet 4. translate language

--- osu.Game.Rulesets.Karaoke/Wiki/Sections/Pieces/MultiSettingsEnumDropdown.cs
+++ osu.Game.Rulesets.Karaoke/Wiki/Sections/Pieces/MultiSettingsEnumDropdown.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Overlays.Settings;
+using system.Collection.Generic;
+
 
 namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
 {
@@ -11,11 +13,60 @@ namespace osu.Game.Rulesets.Karaoke.Wiki.Sections.Pieces
     /// Filter cannot select same
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class MultiSettingsEnumDropdown<T> : FillFlowContainer<SettingsEnumDropdown<T>>
+    public class MultiSettingsEnumDropdown<T> : FillFlowContainer
     {
+        //List selection
+        protected List<DropDownSection<T>> ListSelectin = new List<DropDownSection<T>>();
+
+
+        public string AlretMessage { get; set; } = nameof(T) + "Cannot be same";
+
+        public int StartSelectionNumber { get; set; } = 1;
+
+        public int MaxSelextionNumber { get; set } = -1;
+
+        public bool EnableAddMore { get; set; } = true;
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
         public MultiSettingsEnumDropdown()
         {
 
         }
+
+        /// <summary>
+        /// Loads the complete.
+        /// </summary>
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            //TODO : add section in here
+        }
     }
+
+    /// <summary>
+    /// Drop down section.
+    /// </summary>
+    public class DropDownSelectionSection<T> : FillFlowContainer
+    {
+        
+    }
+
+    /// <summary>
+    /// Alert Selection
+    /// </summary>
+    public class AlertSection : FillFlowContainer
+    {
+        
+    }
+
+    /// <summary>
+    /// Add more drop down selection section. button
+    /// </summary>
+    public class AddMoreDropDownSelectionSection : FillFlowContainer
+    {
+        
+    }
+
 }
