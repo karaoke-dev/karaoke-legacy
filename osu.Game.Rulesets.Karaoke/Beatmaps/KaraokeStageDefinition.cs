972e39f515b67424fd75827a7e9b137e16b11ac9
為什麼 <andy840119@gmail.com>
2018-05-16 17:52:37 +0800

    加上一些物件 供之後修改

---
+++ osu.Game.Rulesets.Karaoke/Beatmaps/KaraokeStageDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Game.Rulesets.Karaoke.UI.Layers.Note;
+
+namespace osu.Game.Rulesets.Karaoke.Beatmaps
+{
+    public class KaraokeStageDefinition
+    {
+        /// <summary>
+        /// The number of <see cref="Column"/>s which this stage contains.
+        /// </summary>
+        public int Columns;
+
+        /// <summary>
+        /// Whether the column index is a special column for this stage.
+        /// </summary>
+        /// <param name="column">The 0-based column index.</param>
+        /// <returns>Whether the column is a special column.</returns>
+        public bool IsSpecialColumn(int column) => Columns % 2 == 1 && column == Columns / 2;
+    }
+}
---
+++ osu.Game.Rulesets.Karaoke/UI/Layers/Note/KaraokeToneStage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using osu.Framework.Allocation;
+using osu.Framework.Configuration;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Game.Rulesets.Judgements;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI.Scrolling;
+using osu.Game.Screens.Play;
+
+namespace osu.Game.Rulesets.Karaoke.UI.Layers.Note
+{
+    public class KaraokeToneStage : ScrollingPlayfield
+    {
+        /// <summary>
+        /// Whether this playfield should be inverted. This flips everything inside the playfield.
+        /// </summary>
+        public readonly Bindable<bool> Inverted = new Bindable<bool>(true);
+
+        public List<SquareGraph.Column> Columns => stages.SelectMany(x => x.Columns).ToList();
+        private readonly List<KaraokeStage> stages = new List<KaraokeStage>();
+
+        public KaraokeToneStage(List<KaraokeStageDefinition> stageDefinitions)
+            : base(ScrollingDirection.Up)
+        {
+            if (stageDefinitions == null)
+                throw new ArgumentNullException(nameof(stageDefinitions));
+
+            if (stageDefinitions.Count <= 0)
+                throw new ArgumentException("Can't have zero or fewer stages.");
+
+            Inverted.Value = true;
+
+            GridContainer playfieldGrid;
+            InternalChild = playfieldGrid = new GridContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Content = new[] { new Drawable[stageDefinitions.Count] }
+            };
+
+
+            int firstColumnIndex = 0;
+            for (int i = 0; i < stageDefinitions.Count; i++)
+            {
+                var newStage = new KaraokeStage(firstColumnIndex, stageDefinitions[i]);
+                newStage.VisibleTimeRange.BindTo(VisibleTimeRange);
+                newStage.Inverted.BindTo(Inverted);
+
+                playfieldGrid.Content[0][i] = newStage;
+
+                stages.Add(newStage);
+                AddNested(newStage);
+
+                firstColumnIndex += newStage.Columns.Count;
+            }
+        }
+
+        public override void Add(DrawableHitObject h) => getStageByColumn(((ManiaHitObject)h.HitObject).Column).Add(h);
+
+        public void Add(BarLine barline) => stages.ForEach(s => s.Add(barline));
+
+        private KaraokeStage getStageByColumn(int column)
+        {
+            int sum = 0;
+            foreach (var stage in stages)
+            {
+                sum = sum + stage.Columns.Count;
+                if (sum > column)
+                    return stage;
+            }
+
+            return null;
+        }
+
+        [BackgroundDependencyLoader]
+        private void load(ManiaConfigManager maniaConfig)
+        {
+            maniaConfig.BindWith(ManiaSetting.ScrollTime, VisibleTimeRange);
+        }
+
+        internal void OnJudgement(DrawableHitObject judgedObject, Judgement judgement)
+        {
+            getStageByColumn(((ManiaHitObject)judgedObject.HitObject).Column).OnJudgement(judgedObject, judgement);
+        }
+    }
+}
