584c31af34d5e9e4953ae8de68bc47a5631d258a
為什麼 <andy840119@gmail.com>
2018-05-21 01:06:07 +0900

    commit

---
+++ osu.Game.Rulesets.Karaoke.Develop/DevelopNoteUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using osu.Game.Tests.Visual;
+
+namespace osu.Game.Rulesets.Karaoke.Develop
+{
+    [TestFixture]
+    [Ignore("getting CI working")]
+    [System.ComponentModel.Description("Test Karaoke Object")]
+    public class DevelopNoteUi : OsuTestCase
+    {
+        public DevelopNoteUi()
+        {
+
+        }
+    }
+}
