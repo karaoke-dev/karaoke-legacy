01fe3c78bcb28bbe3e99d90b91d53fbfbc9845c2
為什麼 <andy840119@gmail.com>
2018-06-25 00:39:50 +0900

    create timeline point

---
+++ osu.Game.Rulesets.Karaoke/Objects/TimeLine/TimeLinePoint.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace osu.Game.Rulesets.Karaoke.Objects.TimeLine
+{
+    public class TimeLinePoint : IComparable , IEquatable<TimeLinePoint>
+    {
+        [JsonIgnore]
+        public static int MaxCharNumber = 10;
+
+        public int Index { get; set; }
+
+        public float? Percentage { get; set; }
+
+        public TimeLinePoint()
+        {
+
+        }
+
+        public TimeLinePoint(int index, float? percentage = null)
+        {
+            Index = index;
+            Percentage = percentage;
+        }
+
+        /// <summary>
+        /// Comrare
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public int CompareTo(object obj)
+        {
+            if (obj is TimeLinePoint point)
+            {
+                return this.GetHashCode() - point.GetHashCode();
+            }
+            throw new InvalidCastException(nameof(obj) + " is not "+ nameof(TimeLinePoint));
+        }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(TimeLinePoint other)
+        {
+            if (other?.Index == Index && other.Percentage == Index)
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Equals
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TimeLinePoint)obj);
+        }
+
+        /// <summary>
+        /// Generate hash code
+        /// Used to compare value
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Index * MaxCharNumber) + (int)((Percentage ?? 1) * MaxCharNumber);
+            }
+        }
+    }
+}
