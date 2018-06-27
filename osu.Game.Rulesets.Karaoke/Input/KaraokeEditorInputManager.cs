e8d1ddd09d16879c1aba0dbc7579e72a9e2507eb
為什麼 <andy840119@gmail.com>
2018-06-27 18:35:09 +0800

    1. merge into one file 2. karaoke editor will use KaraokeEditorInputManager with KaraokeEditorKeyAction

--- osu.Game.Rulesets.Karaoke/Input/KaraokeEditorInputManager.cs
+++ osu.Game.Rulesets.Karaoke/Input/KaraokeEditorInputManager.cs
@@ -1,16 +1,33 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.ComponentModel;
 using osu.Framework.Input.Bindings;
 using osu.Game.Rulesets.UI;
 
 namespace osu.Game.Rulesets.Karaoke.Input
 {
-    public class KaraokeEditorInputManager : RulesetInputManager<KaraokeKeyAction>
+    /// <summary>
+    /// Karaoke's editor input manager
+    /// <see cref="KaraokeKeyAction"/> cannot be work in editor
+    /// </summary>
+    public class KaraokeEditorInputManager : RulesetInputManager<KaraokeEditorKeyAction>
     {
         public KaraokeEditorInputManager(RulesetInfo ruleset)
             : base(ruleset, 1, SimultaneousBindingMode.Unique)
         {
         }
+    }
+
+    public enum KaraokeEditorKeyAction
+    {
+        //open Dialog Hotkey
+        [Description("Template Dialog")] TemplateDialog, //T
+
+        [Description("Lyrics Dialog")] LyricsDialog, //L
+
+        [Description("Translate Dialog")] TranslateDialog, //R
+
+        [Description("Singer Dialog")] SingerDialog //P
     }
 }
--- osu.Game.Rulesets.Karaoke/Input/KaraokeInputManager.cs
+++ osu.Game.Rulesets.Karaoke/Input/KaraokeInputManager.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System.ComponentModel;
 using osu.Framework.Input.Bindings;
 using osu.Game.Rulesets.UI;
 
@@ -11,6 +12,86 @@ namespace osu.Game.Rulesets.Karaoke.Input
         public KaraokeInputManager(RulesetInfo ruleset)
             : base(ruleset, 0, SimultaneousBindingMode.Unique)
         {
+
         }
+    }
+
+    public enum KaraokeKeyAction
+    {
+        //KaraokeGamePlay
+        [Description("First BaseLyric")] FirstLyric, // 1 ,number_1
+
+        [Description("Previous BaseLyric")] PreviousLyric, // left
+
+        [Description("Next BaseLyric")] NextLyric, //right
+
+        [Description("PlayAndPause")] PlayAndPause, //space
+
+        [Description("Increase Speed")] IncreaseSpeed, //Q
+
+        [Description("Decrease Speed")] DecreaseSpeed, //A
+
+        [Description("Reset Speed")] ResetSpeed, //Z
+
+        [Description("Increase Tone")] IncreaseTone, //W
+
+        [Description("Decrease Tone")] DecreaseTone, //S
+
+        [Description("Reset Tone")] ResetTone, //X
+
+        [Description("Increase BaseLyric Appear Time")] IncreaseLyricAppearTime, //E
+
+        [Description("Decrease BaseLyric Appear Time")] DecreaseLyricAppearTime, //D
+
+        [Description("Reset BaseLyric Appear Time")] ResetLyricAppearTime, //C
+
+        [Description("Panel key")] OpenPanel //P
+    }
+
+    /// <summary>
+    ///     X or Y-anix can be use as...
+    /// </summary>
+    public enum KaraokeScrollAction
+    {
+        /// <summary>
+        ///     Time
+        /// </summary>
+        Time,
+
+        /// <summary>
+        ///     Volumn
+        /// </summary>
+        Volumn,
+
+        /// <summary>
+        ///     Dim
+        /// </summary>
+        BackgroundDim,
+
+        /// <summary>
+        ///     Tone
+        /// </summary>
+        Tome,
+
+        /// <summary>
+        ///     Speed
+        /// </summary>
+        Speed
+    }
+
+    /// <summary>
+    ///     action
+    /// </summary>
+    public enum KaraokeTapAction
+    {
+        /// <summary>
+        ///     Tap to pause
+        /// </summary>
+        Pause,
+
+        /// <summary>
+        ///     Show panel
+        /// </summary>
+        ShowPanel
     }
 }
