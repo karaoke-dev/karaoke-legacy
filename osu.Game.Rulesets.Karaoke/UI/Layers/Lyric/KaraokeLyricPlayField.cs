ba6093f6cd50d6a16673c999666fa10764bd062c
為什麼 <andy840119@gmail.com>
2018-08-13 02:35:33 +0900

    目前把mask問題修正好了 準備合併回master

--- osu.Game.Rulesets.Karaoke/UI/Layers/Lyric/KaraokeLyricPlayField.cs
+++ osu.Game.Rulesets.Karaoke/UI/Layers/Lyric/KaraokeLyricPlayField.cs
@@ -1,12 +1,15 @@
 ﻿// Copyright (c) 2007-2018 ppy Pty Ltd <contact@ppy.sh>.
 // Licensed under the MIT Licence - https://raw.githubusercontent.com/ppy/osu/master/LICENCE
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Caching;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Game.Rulesets.Karaoke.Configuration;
+using osu.Game.Rulesets.Karaoke.Extension;
 using osu.Game.Rulesets.Karaoke.Objects;
 using osu.Game.Rulesets.Karaoke.Objects.Drawables.Lyric;
 using osu.Game.Rulesets.Karaoke.Objects.Drawables.Lyric.Types;
@@ -57,23 +60,146 @@ namespace osu.Game.Rulesets.Karaoke.UI.Layers.Lyric
 
         protected override HitObjectContainer CreateHitObjectContainer() => new LyricPlayFieldContainer();
 
-        private class LyricPlayFieldContainer : HitObjectContainer
+        public class LyricPlayFieldContainer : HitObjectContainer
         {
+            public List<float> LineSpacing { get; set; }
+            private Cached layout = new Cached();
+            protected void InvalidateLayout() => layout.Invalidate();
 
-            public readonly LyricLagacyLayoutContainer LyricLagacyLayoutContainer;
             public LyricPlayFieldContainer()
             {
-                InternalChild = LyricLagacyLayoutContainer = new LyricLagacyLayoutContainer()
+                LineSpacing = new List<float>()
                 {
-                    RelativeSizeAxes = Axes.Both
+                    0,100
                 };
             }
 
-            public override IEnumerable<DrawableHitObject> Objects => LyricLagacyLayoutContainer.Children.Cast<DrawableHitObject>().OrderBy(h => h.HitObject.StartTime);
-            public override IEnumerable<DrawableHitObject> AliveObjects => LyricLagacyLayoutContainer.FlowingChildren.Cast<DrawableHitObject>().OrderBy(h => h.HitObject.StartTime);
+            public override void Add(DrawableHitObject hitObject)
+            {
+                base.Add(hitObject);
 
-            public override void Add(DrawableHitObject hitObject) => LyricLagacyLayoutContainer.Add(hitObject as DrawableLyric);
-            public override bool Remove(DrawableHitObject hitObject) => LyricLagacyLayoutContainer.Remove(hitObject as DrawableLyric);
+                if (IsLoaded)
+                    ComputeLyricLayout();
+            }
+
+            public override bool Remove(DrawableHitObject hitObject)
+            {
+                var remove = base.Remove(hitObject);
+
+                if (IsLoaded)
+                    ComputeLyricLayout();
+
+                return remove;
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+                ComputeLyricLayout();
+            }
+
+            /// <summary>
+            /// Set object's margin and padding(left and right)
+            /// </summary>
+            protected virtual void ComputeLyricLayout()
+            {
+                for (int i = 0; i < InternalChildren.Count; i++)
+                {
+                    var lyric = InternalChildren[i] as DrawableLyric;
+                    var layoutIndex = i % LineSpacing.Count;
+
+                    if (lyric != null)
+                    {
+                        lyric.DisplayLayer = layoutIndex;
+                        lyric.Padding
+                            = new MarginPadding { Left = LineSpacing[layoutIndex], Right = 100 };
+                    }
+                }
+            }
+
+            public virtual void ComputeLayout()
+            {
+                foreach (var child in InternalChildren)
+                {
+                    if (child is DrawableLyric drawableLyric)
+                    {
+                        if (drawableLyric.DisplayLayer != 0)
+                        {
+                            var pervous = InternalChildren.GetPrevious(drawableLyric);
+                            var next = InternalChildren.GetNext(drawableLyric);
+                            var perviousHeight = pervous?.Y + pervous?.DrawHeight ?? 0;
+                            var nextHeight = next?.Y + next?.DrawHeight ?? 0;
+                            drawableLyric.Y = Math.Max(perviousHeight, nextHeight);
+                        }
+                    }
+                }
+            }
+
+            #region FlowContainer
+
+            protected override bool RequiresChildrenUpdate => base.RequiresChildrenUpdate || !layout.IsValid;
+
+            private readonly Dictionary<Drawable, float> layoutChildren = new Dictionary<Drawable, float>();
+
+            protected override void AddInternal(Drawable drawable)
+            {
+                layoutChildren.Add(drawable, 0f);
+                // we have to ensure that the layout gets invalidated since Adding or Removing a child will affect the layout. The base class will not invalidate
+                // if we are set to AutoSizeAxes.None, but even in that situation, the layout can and often does change when children are added/removed.
+                InvalidateLayout();
+                base.AddInternal(drawable);
+            }
+
+            protected override bool RemoveInternal(Drawable drawable)
+            {
+                layoutChildren.Remove(drawable);
+                // we have to ensure that the layout gets invalidated since Adding or Removing a child will affect the layout. The base class will not invalidate
+                // if we are set to AutoSizeAxes.None, but even in that situation, the layout can and often does change when children are added/removed.
+                InvalidateLayout();
+                return base.RemoveInternal(drawable);
+            }
+
+            protected override void ClearInternal(bool disposeChildren = true)
+            {
+                layoutChildren.Clear();
+                // we have to ensure that the layout gets invalidated since Adding or Removing a child will affect the layout. The base class will not invalidate
+                // if we are set to AutoSizeAxes.None, but even in that situation, the layout can and often does change when children are added/removed.
+                InvalidateLayout();
+                base.ClearInternal(disposeChildren);
+            }
+
+            protected override bool UpdateChildrenLife()
+            {
+                bool changed = base.UpdateChildrenLife();
+
+                if (changed)
+                    InvalidateLayout();
+
+                return changed;
+            }
+
+            public override void InvalidateFromChild(Invalidation invalidation, Drawable source = null)
+            {
+                //Colour captures potential changes in IsPresent. If this ever becomes a bottleneck,
+                //Invalidation could be further separated into presence changes.
+                if ((invalidation & (Invalidation.RequiredParentSizeToFit | Invalidation.Colour)) > 0)
+                    InvalidateLayout();
+
+                base.InvalidateFromChild(invalidation, source);
+            }
+
+            protected override void UpdateAfterChildren()
+            {
+                base.UpdateAfterChildren();
+
+                if (!layout.IsValid)
+                {
+                    ComputeLayout();
+                    layout.Validate();
+                }
+            }
+
+            #endregion
         }
     }
 }
