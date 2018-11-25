981baf8e8a2bd31f8d63f944b9f4073b0e0b0a9c
andy840119 <andy840119@gmail.com>
2018-11-25 12:46:27 +0900

    fixed

---
+++ osu.Game.Rulesets.Karaoke/UI/KaroakeNoteScrollDirection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.Game.Rulesets.UI.Scrolling;
+
+namespace osu.Game.Rulesets.Karaoke.UI
+{
+    public enum KaroakeNoteScrollDirection
+    {
+        Left = ScrollingDirection.Left,
+        Right = ScrollingDirection.Right
+    }
+}
